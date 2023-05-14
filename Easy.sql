1e585b83eea75f00638e447277cfc3e043b4716c
Luke Starnes <123986345+LukeStarnes@users.noreply.github.com>
2023-05-14 21:53:28 +0100

    Update Easy.sql

--- Easy.sql
+++ Easy.sql
@@ -16,3 +16,23 @@ LEFT JOIN page_likes
 ON pages.page_id = page_likes.page_id
 WHERE page_likes.liked_date IS NULL
 ORDER BY page_id
+
+--Tesla is investigating production bottlenecks and they need your help to extract the relevant data. 
+--Write a query that determines which parts with the assembly steps have initiated the assembly process but remain unfinished.
+--Assumptions:
+----parts_assembly table contains all parts currently in production, each at varying stages of the assembly process.
+----An unfinished part is one that lacks a finish_date.
+SELECT part, assembly_step
+FROM parts_assembly
+WHERE finish_date IS NULL
+
+--Assume you're given a table Twitter tweet data, write a query to obtain a histogram of tweets posted per user in 2022. 
+--Output the tweet count per user as the bucket and the number of Twitter users who fall into that bucket.
+--In other words, group the users by the number of tweets they posted in 2022 and count the number of users in each group.
+SELECT NumberOfTweetsPerUser AS tweet_bucket, Count(*) AS users_num
+FROM
+  (SELECT user_id, COUNT(tweet_id) AS NumberOfTweetsPerUser
+  FROM tweets
+  WHERE tweet_date BETWEEN '2022-01-01' AND '2022-12-31'
+  GROUP BY user_id) AS total_tweets
+GROUP BY NumberOfTweetsPerUser
