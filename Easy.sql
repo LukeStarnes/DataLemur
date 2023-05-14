d7644c6d75230631b3a8e5de5b97cff1d5023409
Luke Starnes <123986345+LukeStarnes@users.noreply.github.com>
2023-05-14 21:34:52 +0100

    Update Easy.sql

--- Easy.sql
+++ Easy.sql
@@ -7,3 +7,12 @@ WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
 GROUP BY candidate_id
 HAVING count(*) = 3
 ORDER BY candidate_id
+
+--Assume you're given the tables below about Facebook Page and Page likes (as in "Like a Facebook Page").
+--Write a query to return the IDs of the Facebook pages which do not possess any likes. The output should be sorted in ascending order.
+SELECT pages.page_id
+FROM pages
+LEFT JOIN page_likes
+ON pages.page_id = page_likes.page_id
+WHERE page_likes.liked_date IS NULL
+ORDER BY page_id
