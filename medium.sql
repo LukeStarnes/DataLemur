ce0b5aeead5adc42fd6cae590d781b19098d801b
Luke Starnes <123986345+LukeStarnes@users.noreply.github.com>
2023-05-22 20:21:21 +0100

    Update medium.sql

--- medium.sql
+++ medium.sql
@@ -18,3 +18,32 @@ SELECT category, product, total_spend
 FROM top_spend
 WHERE ranking <= 2
 ORDER BY category, total_spend DESC;
+
+
+--Assume there are three Spotify tables containing information about the artists, songs, and music charts. 
+--Write a query to find the top 5 artists whose songs appear most frequently in the Top 10 of the global_song_rank table.
+--Display the top 5 artist names in ascending order, along with their song appearance ranking. 
+--Note that if two artists have the same number of song appearances, they should have the same ranking, and the rank numbers should be continuous 
+--(i.e. 1, 2, 2, 3, 4, 5).
+--For instance, if Ed Sheeran appears in the Top 10 five times and Bad Bunning four times, Ed Sheeran should be ranked 1st, 
+--and Bad Bunny should be ranked 2nd.
+WITH cte AS (SELECT artist_id, count(*) AS top_ten_charts
+FROM songs
+JOIN global_song_rank
+ON songs.song_id = global_song_rank.song_id
+WHERE global_song_rank.rank <=10
+GROUP BY artist_id
+ORDER BY top_ten_charts DESC),
+
+cte2 AS (SELECT *, DENSE_RANK() 
+OVER(ORDER BY top_ten_charts DESC)
+AS artist_rank
+FROM cte)
+
+SELECT artist_name, artist_rank
+FROM cte2
+JOIN artists
+ON cte2.artist_id = artists.artist_id
+WHERE artist_rank <=5
+ORDER BY artist_rank, artist_name
+
